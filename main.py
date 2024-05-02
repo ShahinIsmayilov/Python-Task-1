8a86a15555aaadd18f1b9013abb64a465019b8c0
Şahin İsmayılov <157419313+ismylowss@users.noreply.github.com>
2024-05-03 00:27:41 +0400

    Python

---
+++ main.py
@@ -0,0 +1,40 @@
+"Task1"
+# ədədlər = []
+# for i in range(5):
+#     ədəd = int(input(f"{i+1}: ədəd daxil edin: "))
+#     ədədlər.append(ədəd)
+
+# ədədlər.sort()
+# print(ədədlər)
+
+"Task2"
+# cümlə = input("Bir cümlə daxil edin: ")
+# hərflər = list(cümlə)
+# hərflər.sort()
+
+# əlifbasırası = ""
+# for hərf in hərflər:
+#     əlifbasırası += hərf
+    
+# print(əlifbasırası)
+
+"Task3"
+# n = 13
+# cəhd_sayı = 0
+# while True:
+#     cəhd = input("Ədəd daxil edin: ")
+#     cəhd_sayı += 1
+   
+#     if int(cəhd) == n:
+#         print(f"Təbriklər! {cəhd_sayı} cəhdə tapdınız.")
+#         break
+
+"Task4"
+# for ədəd in range(1, 100):
+#     sadə = 1
+#     for bölən in range(2, ədəd):
+#         if ədəd % bölən == 0:
+#             sadə = 0
+#             break
+#     if sadə:
+#         print(ədəd)
